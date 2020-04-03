5dec90f1553138e25b47cbd74533b209ac385408
vking34 <lzzvkingzzl@gmail.com>
2020-04-03 16:02:59 +0700

    init(chap00): main thread

---
+++ src/chapter00/README.md
@@ -0,0 +1,5 @@
+## Chapter 01: Main Thread
+
+1. A program can have multiple processes.
+2. A process can have multiple threads.
+3. The single thread that starts by default is called "Main Thread".
---
+++ src/chapter01/MainThread.java
@@ -0,0 +1,15 @@
+package chapter01;
+
+public class MainThread {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread t = Thread.currentThread();
+        System.out.println("Current Thread: " + t.getName());
+
+        Thread.sleep(3_000);
+
+        t.setName("my-thread");
+        System.out.println("Current Thread: " + t.getName());
+    }
+}
