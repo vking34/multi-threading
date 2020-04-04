e9624dcd4cb0e6486f06168e5e8c76b74d10b364
vking34 <lzzvkingzzl@gmail.com>
2020-04-04 14:26:35 +0700

    feat(chapter06): add time check

--- src/chapter06/StringReverseThread.java
+++ src/chapter06/StringReverseThread.java
@@ -7,24 +7,30 @@ public class StringReverseThread extends Thread {
     private List<Character> names;
     private String message;
 
-    StringReverseThread(List<Character> names, String message){
+    StringReverseThread(List<Character> names, String message) {
         this.message = message;
         this.names = names;
     }
 
     @Override
-    public void run(){
-        synchronized (this.names) {
-            try {
+    public void run() {
+        long startTime = System.currentTimeMillis();
+        try {
+            synchronized (this.names) {
                 for (int i = 0; i < this.message.length(); i++) {
                     this.names.add(this.message.charAt(i));
                     Thread.sleep(50);
                 }
-            } catch (InterruptedException e) {
-                System.out.println(e.toString());
+
+                System.out.println(names);
             }
 
-            System.out.println(names);
+            Thread.sleep(300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+        long timeTaken = System.currentTimeMillis() - startTime;
+        System.out.println("Time taken of " + Thread.currentThread().getName() + ": " + timeTaken);
     }
 }
